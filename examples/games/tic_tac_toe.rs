3fe30f19b67decc7e978a7352c1f0ab1b7523fcb
Alice Cecile <alice.i.cecile@gmail.com>
2022-07-28 17:21:33 -0400

    Add schedule-in-exclusive-system boilerplate

--- examples/games/tic_tac_toe.rs
+++ examples/games/tic_tac_toe.rs
@@ -1,52 +1,78 @@
 //! Demonstrates how to make turn-based games in Bevy,
-//! using an extremely simple tic-tac-toe game
-
-use std::cell::Cell;
+//! using a tic-tac-toe game as the base.
+//!
+//! The thing to pay attention to here is the use of an exclusive system that runs a schedule.
 
 use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
+        .add_startup_system(ui::setup)
+        // Runs all of our logic in a sub-schedule
+        // which is scheduled at the end of CoreStage::Update by default
+        .add_system(logic::run_schedule.exclusive_system())
         .run();
 }
 
-#[derive(Component)]
-enum CellContents {
-    Empty,
-    X,
-    O,
+mod logic {
+    #[derive(StageLabel)]
+    enum TurnBasedStage {
+        Picking,
+        Checking,
+    }
+
+    use bevy::prelude::*;
+
+    /// Resource that stores all of our game logic
+    #[derive(Deref, DerefMut)]
+    struct TurnBasedSchedule(Schedule);
+
+    impl Default for TurnBasedSchedule {
+        fn default() -> Self {
+            let update_stage = SystemStage::parallel();
+            // Add systems here to update_stage
+
+            let mut schedule = Schedule::default();
+            schedule.add_stage(TurnBasedStage::Picking, update_stage);
+
+            TurnBasedSchedule(schedule)
+        }
+    }
+
+    pub fn run_schedule(world: &mut World) {
+        // Split apart the SimSchedule and the rest of the world
+        world.resource_scope(|world, mut sim_schedule: Mut<TurnBasedSchedule>| {
+            sim_schedule.run_once(world);
+        });
+    }
 }
 
-#[derive(Component)]
-struct Position {
-    x: u8,
-    y: u8,
-}
+mod ui {
+    use bevy::prelude::*;
 
-/// Draw the grid as UI elements
-fn setup(mut commands: Commands) {
-    // UI camera
-    commands.spawn_bundle(Camera2dBundle::default());
+    #[derive(Component)]
+    enum CellContents {
+        Empty,
+        X,
+        O,
+    }
 
-    // Create a root UI node
-    let root = commands
-        .spawn_bundle(NodeBundle {
-            style: Style {
-                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                ..default()
-            },
-            ..default()
-        })
-        .id();
+    #[derive(Component)]
+    struct Position {
+        x: u8,
+        y: u8,
+    }
 
-    for x in 0..3 {
-        // Create a node for each column
-        let column = commands
+    /// Draw the grid as UI elements
+    pub fn setup(mut commands: Commands) {
+        // UI camera
+        commands.spawn_bundle(Camera2dBundle::default());
+
+        // Create a root UI node
+        let root = commands
             .spawn_bundle(NodeBundle {
                 style: Style {
-                    flex_direction: FlexDirection::Column,
                     size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
                     ..default()
                 },
@@ -54,26 +80,40 @@ fn setup(mut commands: Commands) {
             })
             .id();
 
-        commands.entity(root).add_child(column);
-
-        for y in 0..3 {
-            // Create three cells in each column
-            let cell = commands
+        for x in 0..3 {
+            // Create a node for each column
+            let column = commands
                 .spawn_bundle(NodeBundle {
-                    color: Color::WHITE.into(),
                     style: Style {
+                        flex_direction: FlexDirection::Column,
                         size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
                         ..default()
                     },
                     ..default()
                 })
-                // Track where each cell is spawned
-                .insert(Position { x, y })
-                // Store what's in each cell
-                .insert(CellContents::Empty)
                 .id();
 
-            commands.entity(column).add_child(cell);
+            commands.entity(root).add_child(column);
+
+            for y in 0..3 {
+                // Create three cells in each column
+                let cell = commands
+                    .spawn_bundle(NodeBundle {
+                        color: Color::WHITE.into(),
+                        style: Style {
+                            size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                            ..default()
+                        },
+                        ..default()
+                    })
+                    // Track where each cell is spawned
+                    .insert(Position { x, y })
+                    // Store what's in each cell
+                    .insert(CellContents::Empty)
+                    .id();
+
+                commands.entity(column).add_child(cell);
+            }
         }
     }
 }
